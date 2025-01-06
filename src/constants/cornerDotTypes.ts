fca057e205f3ef521d3e0e65ae0fca9de25988b4
MEELAD RAZA <56252911+milad-raza-developer@users.noreply.github.com>
2025-01-06 11:49:38 +0500

    forx again with qr code styling

--- src/constants/cornerDotTypes.ts
+++ src/constants/cornerDotTypes.ts
@@ -2,5 +2,16 @@ import { CornerDotTypes } from "../types";
 
 export default {
   dot: "dot",
-  square: "square"
+  square: "square",
+  squareGrid: "square-grid",
+  squareRounded: "square-rounded",
+  rightBottomSquare: "square-right-bottom",
+  leftTopCircle: "circle-left-top",
+  rightBottomCircle: "circle-right-bottom",
+  diamond: "diamond",
+  star: "star",
+  plus: "plus",
+  cross: "cross",
+  rhombus: "rhombus",
+  leaf: "leaf"
 } as CornerDotTypes;
